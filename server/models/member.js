6d372aad47569931721119c9b0e3c4b1e6b669b5
Noah Cain <noahcain90@yahoo.com>
2023-01-26 15:26:44 -0800

    updated Schema layout

--- server/models/member.js
+++ server/models/member.js
@@ -1,37 +1,39 @@
-const mongoose = require( 'mongoose' );
-const schemaMembers = mongoose.Schema( {
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: false
-    },
-    phoneNumber: {
-        type: Number,
-        required: true
-    },
-    birthday: {
-        type: String,
-        required: false
-    },
-    occupation: {
-        type: String,
-        required: false
-    }
-} );
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-module.exports = mongoose.model('Members', schemaMembers)
+const schemaMembers = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+  birthday: {
+    type: String,
+    required: false,
+  },
+  occupation: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model('Members', schemaMembers);
